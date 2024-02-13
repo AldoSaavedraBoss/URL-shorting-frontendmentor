ab21b9f150eae6b36bd5c79931a7919440492657
AldoSaavedraBoss <aldo.saavedra.cervantes@gmail.com>
2024-02-13 11:07:25 -0600

    stting config and workflow to deply

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://aldosaavedraboss.github.io',
+  base: '/URL-shorting-frontendmentor'
 });
