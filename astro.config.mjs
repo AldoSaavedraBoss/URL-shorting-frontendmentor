b40b2edc84f02fc6c3d458106fa34ada1b5c08b7
AldoSaavedraBoss <aldo.saavedra.cervantes@gmail.com>
2024-02-13 11:15:25 -0600

    removing base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://aldosaavedraboss.github.io',
-  base: '/URL-shorting-frontendmentor'
 });
