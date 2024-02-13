8220e81bfc451d10f6641e8c1c5c3e42ccf4a2de
AldoSaavedraBoss <aldo.saavedra.cervantes@gmail.com>
2024-02-13 11:39:09 -0600

    changing images' path

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://aldosaavedraboss.github.io',
+  base: '/URL-shorting-frontendmentor'
 });
